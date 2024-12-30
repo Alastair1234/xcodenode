37b17bfc47821b6967beb57b9fa796fbe6909e46
Alastair1234 <alastacat@gmail.com>
2024-12-30 14:54:14 +0000

    Initial Commit

---
+++ TendonApp/TendonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TendonAppApp.swift
+//  TendonApp
+//
+//  Created by Alastair Catto on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TendonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
